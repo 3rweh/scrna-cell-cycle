b45bc112ed74a38e2bb32a42b7952f399b685ef4
ehugos <hugo.swenson@gmail.com>
2018-12-13 11:31:39 +0100

    fixed the myord script ... again

--- r_scripts/hugo/reCAT_master/R/get_myord.r
+++ r_scripts/hugo/reCAT_master/R/get_myord.r
@@ -6,7 +6,7 @@ get_myord <- function(cycle_start, ordIndex)
   # If so, it sets the direction in reverse direction, if not, it is set to forward direction
   
 	if(cycle_start >= (length(ordIndex)/2))
-	#	{
+	{
     # Reverse_direction
 		myord = c(cycle_start:1, length(ordIndex):cycle_start)
 		myord <- head(myord, -1)
