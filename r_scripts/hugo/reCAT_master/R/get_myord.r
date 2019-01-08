245f42de3d5fcb174be87e2678575e15c9261735
ehugos <hugo.swenson@gmail.com>
2019-01-08 05:28:17 -0800

    Added working reCAT_master directory

---
+++ r_scripts/hugo/reCAT_master/R/get_myord.r
@@ -0,0 +1,20 @@
+get_myord <- function(cycle_start, ordIndex)
+{
+	# Checks the direction of the cycle (the order and arranges it accordingly)
+  
+	# Looks up if the start of the cycle is larger than, ro equal ot half the length of the ordIndex
+	# If so, it sets the direction in reverse direction, if not, it is set to forward direction
+  
+	# Reverse_direction
+	if (cycle_start >= (length(ordIndex)/2))
+	{
+		myord = c(cycle_start:1, length(ordIndex):cycle_start)
+		myord <- head(myord, -1)
+	
+	# Forward direction
+	} else {
+		myord = c(cycle_start:length(ordIndex), 1:cycle_start)
+		myord <- head(myord, -1)
+	}
+	return(myord)
+}
