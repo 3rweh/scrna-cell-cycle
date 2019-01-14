6ec54085201a7ce592bde829d9d94c3c806fbac7
ehugos <hugo.swenson@gmail.com>
2019-01-14 17:12:14 +0100

    modified both the get_start.r and the get:rdata.r script to work better

--- r_scripts/hugo/reCAT_master/R/get_start.r
+++ r_scripts/hugo/reCAT_master/R/get_start.r
@@ -8,126 +8,93 @@ get_start <- function(score_result, ordIndex)
   s_score <- bayes_cell$S.score
   g2m_score <- bayes_cell$G2M.score
   
-  n_1 = 6
+  n_1 <- 6
   
   get_min_reg <- function(input)
     # Returns the max segment region out of n1 for the input vector 
   {
-    val = -Inf
-    pos = 0
+    val <- -Inf
+    pos <- 0
     inp_splt <- split(input, cut(seq(input), n_1, labels = FALSE))
     reg_vec <- c()
     for (i in 1:length(inp_splt))
     {
       inp_num <- as.numeric(inp_splt[[i]])
-      inp_mean = mean(inp_num)
+      inp_mean <- mean(inp_num)
       reg_vec <- append(reg_vec, inp_mean)
     }
     min_reg <- which.min(reg_vec)
     return (min_reg)
   }
   
-  get_max_reg <- function(input)
-    # Returns the max segment region out of n1 for the input vector 
-  {
-    val = -Inf
-    pos = 0
-    inp_splt <- split(input, cut(seq(input), n_1, labels = FALSE))
-    reg_vec <- c()
-    for (i in 1:length(inp_splt))
-    {
-      inp_num <- as.numeric(inp_splt[[i]])
-      inp_mean = mean(inp_num)
-      reg_vec <- append(reg_vec, inp_mean)
-    }
-    max_reg <- which.max(reg_vec)
-    return (max_reg)
-  }
-  
   get_start_p <- function(g1_score, s_score, g2m_score)
   {
-    # Gets the region where g2m is the smallest (ie, the peak of the g1 phase)
-    g2m_minr <- get_min_reg(g2m_score)
-    # Gets the region where g2m is the largest (ie, the min of the g1 phase)
-    g2m_maxr <- get_max_reg(g2m_score)
-  
-  
-    # Checks the direction of the cycle, if the peak of the g2m phase is in front of the min, it is forward
-    if(g2m_maxr > g2m_minr){
       # Splits all vectors into n_1 regions
       g1_splt <- split(g1_score, cut(seq(g1_score), n_1, labels = FALSE))
       s_splt <- split(s_score, cut(seq(s_score), n_1, labels = FALSE))
       g2m_splt <- split(g2m_score, cut(seq(g2m_score), n_1, labels = FALSE))
     
-      s_vec <- c()
-      for(i in 1:g2m_minr){
-        g1_num = as.numeric(g1_splt[[i]])
-        s_num = as.numeric(s_splt[[i]])
-        g2m_num = as.numeric(g2m_splt[[i]])
-        
-        d_1 = sqrt(sum((g1_num - s_num) ^ 2))
-        d_2 = sqrt(sum((g1_num - g2m_num) ^ 2))
-        d_3 = sqrt(sum((s_num - g2m_num) ^ 2)) 
-        d_tot = d_1 + d_2 + d_3
-        s_vec <- append(s_vec, d_tot) 
-      }
-      # Gets the indices of the n smallest euclidian distances (ie: where g1 meets with g2 and is the closest to s)
-      start_ind <- which.min(s_vec)
-    
-      g1_s_reg = as.numeric(g1_splt[[start_ind]])
-      s_s_reg = as.numeric(s_splt[[start_ind]])
-    
-      # Walks through the min-region, checking if the s-score is larger then that of the g1_score. We know that when the g1_score passes that of the s-score, we have reached the start of the cycle
-      for ( i in 1:length(g1_s_reg)){
-        g1_p_s = g1_s_reg[i]
-        s_p_s = s_s_reg[i]
-        if(g1_p_s > s_p_s){
-          start_pos = i
-          break
-        } else {
-          # If the G1 phase score is always higher then that of the S-phase score,  that means that the start is located in position 1
-          start_pos = 1 
-        }
-      }
-      return(g1_s_reg[[start_pos]])
-    }else if(g2m_minr > g2m_maxr){
-      # Splits all vectors into n_1 regions
-      g1_splt <- rev(split(g1_score, cut(seq(g1_score), n_1, labels = FALSE)))
-      s_splt <- rev(split(s_score, cut(seq(s_score), n_1, labels = FALSE)))
-      g2m_splt <- rev(split(g2m_score, cut(seq(g2m_score), n_1, labels = FALSE)))
-    
-      s_vec <- c()
-      for(i in length(g1_splt):g2m_minr){
-        g1_num = as.numeric(g1_splt[[i]])
-        s_num = as.numeric(s_splt[[i]])
-        g2m_num = as.numeric(g2m_splt[[i]])
+      g2m_minr <- get_min_reg(g2m_score)
       
-        d_1 = sqrt(sum((g1_num - s_num) ^ 2))
-        d_2 = sqrt(sum((g1_num - g2m_num) ^ 2))
-        d_3 = sqrt(sum((s_num - g2m_num) ^ 2)) 
-        d_tot = d_1 + d_2 + d_3
-        s_vec <- append(s_vec, d_tot) 
+      g1_vec <- c()
+      s_vec <- c()
+      g2m_vec <- c()
+      for(i in 1:g2m_minr){
+        g1_num <- as.numeric(g1_splt[[i]])
+        s_num <- as.numeric(s_splt[[i]])
+        g2m_num <- as.numeric(g2m_splt[[i]])
+        
+        d_1 <- sqrt(sum((g1_num - s_num) ^ 2))
+        d_2 <- 2*sqrt(sum((g1_num - g2m_num) ^ 2))
+        d_3 <- sqrt(sum((s_num - g2m_num) ^ 2)) 
+        d_tot <- d_1 + d_2 + d_3
+        g1_vec <- append(g1_vec, g1_num) 
+        s_vec <- append(s_vec, s_num) 
+        g2m_vec <- append(g2m_vec, g2m_num) 
       }
-      # Gets the indices of the n smallest euclidian distances (ie: where g1 meets with g2 and is the closest to s)
-      start_ind <- which.min(s_vec)
-    
-      g1_s_reg = as.numeric(g1_splt[[start_ind]])
-      s_s_reg = as.numeric(s_splt[[start_ind]])
+      
+      g1_vec <- as.numeric(g1_vec) 
+      s_vec <- as.numeric(s_vec) 
+      g2m_vec <- as.numeric(g2m_vec) 
     
       # Walks through the min-region, checking if the s-score is larger then that of the g1_score. We know that when the g1_score passes that of the s-score, we have reached the start of the cycle
-      for (i in length(g1_s_reg):1){
-        g1_p_s = g1_s_reg[i]
-        s_p_s = s_s_reg[i]
-        if(g1_p_s > s_p_s){
-          start_pos = i
+      for ( i in 1:length(g1_vec)-8){
+        g1_p_s <- g1_vec[i]
+        s_p_s <- s_vec[i]
+        g2m_p_s <- g2m_vec[i]
+        
+        g1_p_s_1 <- g1_vec[i+1]
+        s_p_s_1 <- s_vec[i+1]
+        g2m_p_s_1 <- g2m_vec[i+1]
+       
+        g1_p_s_2 <- g1_vec[i+2]
+        s_p_s_2 <- s_vec[i+2]
+        g2m_p_s_2 <- g2m_vec[i+2]
+        
+        g1_p_s_3 <- g1_vec[i+3]
+        s_p_s_3 <- s_vec[i+3]
+        g2m_p_s_3 <- g2m_vec[i+3]
+        
+        g1_p_s_4 <- g1_vec[i+4]
+        s_p_s_4 <- s_vec[i+4]
+        g2m_p_s_4 <- g2m_vec[i+4]
+       
+        g1_p_s_8 <- g1_vec[i+8]
+        s_p_s_8 <- s_vec[i+8]
+        g2m_p_s_8 <- g2m_vec[8]
+        
+        g1_p_m <- mean(g1_p_s + g1_p_s_1 + g1_p_s_2 + g1_p_s_3 + g1_p_s_4 )
+        s_p_m <- mean(s_p_s + s_p_s_1 + s_p_s_2 + s_p_s_3 + s_p_s_4)
+        g2m_p_m <- mean(g2m_p_s + g2m_p_s_1 + g2m_p_s_2 + g2m_p_s_3 + g2m_p_s_4)
+        
+        if(g1_p_s > s_p_s && g1_p_s > g2m_p_s && g1_p_s_1 > s_p_s_1 && g1_p_s_1 > g2m_p_s_1 && g1_p_m > s_p_m && g1_p_m > g2m_p_m){
+          start_pos <- i
           break
-        }else{
-          # If the G1 phase score is always higher then that of the S-phase score,  that means that the start is located in position 1
-          start_pos = length(g1_s_reg)
+        }else if(i == length(g1_s_reg)-8){
+          start_pos <- i+8
         }
       }
-    }
-    return(g1_s_reg[[start_pos]])
+      return(g1_vec[[start_pos]])
   }
 
   start_p <- get_start_p(g1_score, s_score, g2m_score)
--- r_scripts/hugo/reCAT_master/R_backup/get_rdata.r
+++ r_scripts/hugo/reCAT_master/R_backup/get_rdata.r
@@ -1,4 +1,4 @@
-get_rdata <- function(score_result, ordIndex, cycle_start)
+get_rdata <- function(score_result, ordIndex)
 {	
 	# Loads the score results as a separate variable, uses bayes score due to better G2M separation
 	# Arranges these scores with respect to the ordIndex
@@ -8,7 +8,8 @@ get_rdata <- function(score_result, ordIndex, cycle_start)
 	s_score <- bayes_cell$S.score
 	g2m_score <- bayes_cell$G2M.score
 	
-	n_1 = 4
+	n_1 <- 5
+	n_2 <- 4
 	
 	get_max_reg <- function(input, n_val)
 	# Returns the max segment region out of n1 for the input vector 
@@ -27,7 +28,24 @@ get_rdata <- function(score_result, ordIndex, cycle_start)
 		return (max_reg)
 	}
 	
-	get_g <- function(inp, v_2, v_3, reg, cycle_start)
+	get_min_reg <- function(input, n_val)
+	  # Returns the max segment region out of n for the input vector 
+	{
+	  val <- -Inf
+	  pos <- 0
+	  inp_splt <- split(input, cut(seq(input), n_val, labels = FALSE))
+	  reg_vec <- c()
+	  for (i in 1:length(inp_splt))
+	  {
+	    inp_num <- as.numeric(inp_splt[[i]])
+	    inp_mean <- mean(inp_num)
+	    reg_vec <- append(reg_vec, inp_mean)
+	  }
+	  min_reg <- which.min(reg_vec)
+	  return (min_reg)
+	}
+	
+	get_g <- function(inp, v_2, v_3, reg)
 	{
 		if (50 >= length(ordIndex))
 		{
@@ -57,7 +75,7 @@ get_rdata <- function(score_result, ordIndex, cycle_start)
 		    v_3_b_num <- as.numeric(v_3_splt[[(brv_ind[[i]])]])
 		    
 		    d1 <- sqrt(sum((inp_b_num - v_2_b_num) ^ 2))
-		    d2 <- 3*sqrt(sum((inp_b_num - v_3_b_num) ^ 2))
+		    d2 <- 2*sqrt(sum((inp_b_num - v_3_b_num) ^ 2))
 		    
 		    d_tot <- d1+d2
 		    d_vec <- append(d_vec, d_tot) 
@@ -85,24 +103,10 @@ get_rdata <- function(score_result, ordIndex, cycle_start)
 		  v_2_splt <- split(v_2, cut(seq(v_2), n_1, labels = FALSE))
 		  v_3_splt <- split(v_3, cut(seq(v_3), n_1, labels = FALSE))
 		  
-		  # Gets the optimal region
+		  # Retrieves the optimal region for the input vector 
 		  inp_b <- inp_splt[[reg]]
 		  v_2_b <- v_2_splt[[reg]]
 		  v_3_b <- v_3_splt[[reg]]
-		  
-		  ind_r <- match(inp_b, inp)
-		  
-		  # Control sequence for g2m-phase, if the start is close to the location of the g2m-phase, this will limit the region to up until the start of the cycle
-		  if(any(ind_r == cycle_start))
-		  {
-		    ind_s <- which(ind_r == cycle_start)
-		    
-		    inp_b <- inp_b[1:ind_s]
-		    v_2_b <- v_2_b[1:ind_s]
-		    v_3_b <- v_3_b[1:ind_s]
-		  }
-		  
-		  n_2 = ceiling(length(inp_b)/20)
 		  
 		  # Splits the best region into n_2 segments
 		  inp_b_splt <- split(inp_b, cut(seq(inp_b), n_2, labels = FALSE))
@@ -125,7 +129,7 @@ get_rdata <- function(score_result, ordIndex, cycle_start)
 		  }
 		  
 		  # Gets the indices of the n largest euclidian distances
-		  brv_ind <- which(d_vec >= sort(d_vec, decreasing=T)[ceiling(length(inp_b_splt)/2)], arr.ind=TRUE)
+		  brv_ind <- which(d_vec >= sort(d_vec, decreasing=T)[length(inp_b_splt)/2], arr.ind=TRUE)
 		  
 		  # Calculates the mean value of the n regions
 		  br_vec <- c()
@@ -142,55 +146,83 @@ get_rdata <- function(score_result, ordIndex, cycle_start)
 		}
 	}
 	
-	get_s <- function(s_score, g1_score, g2m_score, g1_s)
+	get_s <- function(s_score, g1_score, g2m_score)
 	{
 	  # Function for finding the S-phase of the cycle
+	  # inp = s-score vector
+	  # v_2 = g1-score vector
+	  # v_3= g2m_score vector
 	  
-	  new_s <- s_score[g1_s:length(ordIndex)]
-	  new_g1 <- g1_score[g1_s:length(ordIndex)]
-	  new_g2m <- g2m_score[g1_s:length(ordIndex)]
+	  g1_maxr <- get_max_reg(g1_score, n_2*2)
+	  g1_minr <- get_min_reg(g1_score, n_2*2)
 	  
-	  n_2 <- ceiling(length(new_s)/20)
-	  
-	  s_splt <- split(new_s, cut(seq(new_s), n_2, labels = FALSE))
-	  g1_splt <- split(new_g1, cut(seq(new_g1), n_2, labels = FALSE))
-	  g2m_splt <- split(new_g2m, cut(seq(new_g2m), n_2, labels = FALSE))
+	  # Forward direction
+	  if(g1_minr > g1_maxr){
+	    # Splits all vectors into n_2*4 regions
+	    # Checks the latter 2/3 of the cycle, as the minimum is likely to be in this region
+	    s_splt <- rev(split(s_score, cut(seq(s_score), n_2*4, labels = FALSE)))
+	    g1_splt <- rev(split(g1_score, cut(seq(g1_score), n_2*4, labels = FALSE)))
+	    g2m_splt <- rev(split(g2m_score, cut(seq(g2m_score), n_2*4, labels = FALSE)))
 	    
-	  # Calculates the euclidian distance for the g1 & g2 phase scores based on region
-	  # In the s-phase, the distance for these two should be minimized
-	  d_vec <- c()
-	  for (i in 1:(length(s_splt)))
-	   {
-	    d_tot = 0
-	    s_num <- as.numeric(s_splt[[i]])
-	    g1_num <- as.numeric(g1_splt[[i]])
-	    g2m_num <- as.numeric(g2m_splt[[i]])
+	    # Calculates the euclidian distance for the g1 & g2 phase scores based on region
+	    # In the s-phase, the distance for these two should be minimized
+	    d_vec <- c()
+	    for (i in 1:(length(s_splt)*(2/3)))
+	    {
+	      s_num <- as.numeric(s_splt[[i]])
+	      g1_num <- as.numeric(g1_splt[[i]])
+	      g2m_num <- as.numeric(g2m_splt[[i]])
 	    
-	    d_1 <- 3*sqrt(sum((s_num - g1_num) ^ 2))
-	    d_2 <- sqrt(sum((s_num - g2m_num) ^ 2))
-	    d_3 <- sqrt(sum((g1_num - g2m_num) ^ 2))
-	    d_tot <- d_1 + d_2 + d_3
-      d_vec <- append(d_vec, d_tot) 
-	   }
-	   # Gets the indices of the n smallest euclidian distances (ie: where g1 meets with g2 and is the closest to s)
-	   brv_ind <- which.min(d_vec)
-	   return (as.numeric(s_splt[[brv_ind]]))
+	      d_1 <- 3*sqrt(sum((s_num - g1_num) ^ 2))
+	      d_2 <- sqrt(sum((g1_num - g2m_num) ^ 2))
+	    
+	      d_tot <- d_1 + d_2
+        d_vec <- append(d_vec, d_tot) 
+	    }
+	  }
+	  # Reverse
+	  else if(g1_maxr > g1_minr){
+	    # Splits all vectors into n_2*4 regions
+	    # Checks the first 2/3 of the cycle, as thissi where thes phase should be located
+	    s_splt <- split(s_score, cut(seq(s_score), n_2*4, labels = FALSE))
+	    g1_splt <- split(g1_score, cut(seq(g1_score), n_2*4, labels = FALSE))
+	    g2m_splt <- split(g2m_score, cut(seq(g2m_score), n_2*4, labels = FALSE))
+	      
+	    # Calculates the euclidian distance for the g1 & g2 phase scores based on region
+	    # In the s-phase, the distance for these two should be minimized
+	    d_vec <- c()
+	    for (i in 1:(length(s_splt)*(2/3)))
+	    {
+	      s_num <- as.numeric(s_splt[[i]])
+	      g1_num <- as.numeric(g1_splt[[i]])
+	      g2m_num <- as.numeric(g2m_splt[[i]])
+	      
+	      d_1 <- 3*sqrt(sum((s_num - g1_num) ^ 2))
+	      d_2 <- sqrt(sum((g1_num - g2m_num) ^ 2))
+	      
+	      d_tot <- d_1 + d_2
+	      d_vec <- append(d_vec, d_tot)  
+	    }
+	  }
+	  
+	    # Gets the indices of the n smallest euclidian distances (ie: where g1 meets with g2 and is the closest to s)
+	    brv_ind <- which.min(d_vec)
+	  
+	    return (as.numeric(s_splt[[brv_ind]]))
 	  }
 	
 	g1_r <- get_max_reg(g1_score, n_1)
 	s_r <- get_max_reg(s_score, n_1)
 	g2m_r <- get_max_reg(g2m_score, n_1)
 	
-	g1_b <- get_g(g1_score, s_score, g2m_score, g1_r, cycle_start)
-	g2m_b <- get_g(g2m_score, s_score, g1_score, g2m_r, cycle_start)
+	g1_b <- get_g(g1_score, s_score, g2m_score, g1_r)
+	s_b <- get_s(s_score, g1_score, g2m_score)
+	g2m_b <- get_g(g2m_score, s_score, g1_score, g2m_r)
 	
 	# Matches region-vector against the original vector
 	g1_b_r <- match(g1_b, g1_score)
-	g2m_b_r <- match(g2m_b, g2m_score)
-	
-	g1_s <- min(g1_b_r)
-	s_b <- get_s(s_score, g1_score, g2m_score, g1_s)
 	s_b_r <- match(s_b, s_score)
+	g2m_b_r <- match(g2m_b, g2m_score)
 	
 	# Generates vector with min and max of each parameter
 	g1_rdata <- c(min(g1_b_r), max(g1_b_r))
